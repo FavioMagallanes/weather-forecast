23aca6b86eec7b6a332b8a8656983781b41a70d0
favioMagallanes <magallanes.f@outlook.com>
2023-04-05 11:50:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = (): JSX.Element => {
+  return (
+    <div className="w-[85%] m-auto">
+      <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
